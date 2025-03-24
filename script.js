9c0f606c8fd8434d3f2c8fa5e1ef7ab64d61d53c
Gavin Abramowitz <gavinabramowitz@gmail.com>
2025-03-24 12:46:24 -0400

    draft upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function showFeatured(element) {
+    const title = element.getAttribute("data-title");
+    const description = element.getAttribute("data-description");
+    const imageSrc = element.getAttribute("data-image");
+  
+    document.getElementById("featured-title").textContent = title;
+    document.getElementById("featured-description").textContent = description;
+    document.getElementById("featured-image").src = imageSrc;
+  }
+  
